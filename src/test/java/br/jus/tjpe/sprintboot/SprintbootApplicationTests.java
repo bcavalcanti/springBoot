9d6cad14a6dd08759a7bb64051999b5a818bde71
Bruno Dantas <bcavalcanti@gmail.com>
2020-01-14 14:24:35 -0300

    projeto sprint boot com h2

---
+++ src/test/java/br/jus/tjpe/sprintboot/SprintbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.jus.tjpe.sprintboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
